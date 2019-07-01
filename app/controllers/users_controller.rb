e9622c8e576e4f4a01bd2781c5adf79a44675d92
krbrennan <krb.brennan@gmail.com>
2019-07-01 17:31:52 -0400

    Fixed long-overdue styling issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
+    if (@user.save!) || (@user.save)
       redirect_to links_url
     else
       flash[:errors] = @user.errors.full_messages
